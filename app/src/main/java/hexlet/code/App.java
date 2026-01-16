23db5ee307dd4f865581a733368fb06194ca4a2e
MixsonV <92906194+mixsonv@users.noreply.github.com>
2026-01-16 16:32:51 +0700

    stage 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,32 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-//import picocli.CommandLine.Option;
-//import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true,
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Option(
+            names = {"-f", "--format"},
+            description = "Output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish"
+    )
+    private String format;
+
+    @Parameters(
+            index = "0",
+            description = "Path to the first file"
+    )
+    private String filepath1;
+
+    @Parameters(
+            index = "1",
+            description = "Path to the second file"
+    )
+    private String filepath2;
+
     public void run() {}
 
     public static void main(String[] args) {
