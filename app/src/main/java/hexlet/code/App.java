adc6932cb19c1cf5d866ad92871fca53b787eb69
MixsonV <92906194+mixsonv@users.noreply.github.com>
2026-01-16 16:21:49 +0700

    stage 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,18 @@
 package hexlet.code;
 
-class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+//import picocli.CommandLine.Option;
+//import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    public void run() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
 }
