0b117d234bd3850e29a7498e095bd2fb20d09fbc
MixsonV <92906194+mixsonv@users.noreply.github.com>
2026-01-19 16:23:29 +0700

    stage 6

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,10 +5,12 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+public class App implements Callable<Integer> {
     @Option(
             names = {"-f", "--format"},
             paramLabel = "format",
@@ -31,17 +33,15 @@ public class App implements Runnable {
     )
     private String filePath2;
 
-    public void run() {
+    @Override
+    public Integer call() throws Exception {
         try {
-            switch (format) {
-                case "json":
-                    Differ.generate(filePath1, filePath2);
-                    break;
-                default:
-                    System.out.printf("Unknown format '%s'.%n", format);
-            }
+            String resultDiff = Differ.generate(filePath1, filePath2, format);
+            System.out.println(resultDiff);
+            return 0;
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            return 1;
         }
     }
 
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,20 +7,68 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Differ {
-    public static void generate(String filePath1, String filePath2) throws Exception {
+    public static String generate(String filePath1, String filePath2, String formatName) throws Exception {
+        switch (formatName) {
+            case "json":
+                return jsonDiff(filePath1, filePath2);
+            default:
+                throw new Exception("Unknown format '" + formatName + "'.");
+        }
+    }
+
+    public static String jsonDiff(String filePath1, String filePath2) throws Exception {
         Map<String, Object> data1 = parseJsonFile(readFile(filePath1));
         Map<String, Object> data2 = parseJsonFile(readFile(filePath2));
 
-        System.out.println("First file: " + filePath1);
-        for (Map.Entry<String, Object> entry : data1.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        try {
+            // Получаем все ключи из обоих данных, без повторений
+            Set<String> allKeys = new TreeSet<>();
+            allKeys.addAll(data1.keySet());
+            allKeys.addAll(data2.keySet());
 
-        System.out.println("Second file: " + filePath2);
-        for (Map.Entry<String, Object> entry : data2.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            StringBuilder result = new StringBuilder("{\n");
+
+            for (String key : allKeys) {
+                // Проверяем наличие ключа в обоих data
+                boolean keyInData1 = data1.containsKey(key);
+                boolean keyInData2 = data2.containsKey(key);
+                // Получаем значения для ключа из обоих data
+                String value1 = formatValue(data1.get(key));
+                String value2 = formatValue(data2.get(key));
+
+                if (keyInData1 && keyInData2) {
+                    if (value1.equals(value2)) {
+                        // Без изменений. Значения совпадают.
+                        result.append(String.format("    %s: %s%n", key, value1));
+                    } else {
+                        // Изменение. Значения различаются.
+                        result.append(String.format("  - %s: %s%n", key, value1));
+                        result.append(String.format("  + %s: %s%n", key, value2));
+                    }
+                } else if (keyInData1) {
+                    // Удаление. Ключ есть только в data1.
+                    result.append(String.format("  - %s: %s%n", key, value1));
+                } else {
+                    // Добавление. Ключ есть только в data2.
+                    result.append(String.format("  + %s: %s%n", key, value2));
+                }
+            }
+            result.append("}");
+            return result.toString();
+        } catch (Exception e) {
+            throw new Exception("Error generating JSON diff: " + e.getMessage());
+        }
+    }
+
+    public static String formatValue(Object value) {
+        if (value == null) {
+            return "null";
+        } else {
+            return value.toString();
         }
     }
 
@@ -36,11 +84,10 @@ public class Differ {
     public static String readFile(String filePath) throws Exception {
         try {
             Path path = Paths.get(filePath);
-            if (Files.exists(path)) {
-                return Files.readString(path);
-            } else {
+            if (!Files.exists(path)) {
                 throw new Exception("File '" + filePath + "' not found.");
             }
+            return Files.readString(path);
         } catch (Exception e) {
             throw new Exception("Error reading file: " + e.getMessage());
         }
