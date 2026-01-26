0265968e1d47a43434aec1bed664a45fd72d8392
MixsonV <92906194+mixsonv@users.noreply.github.com>
2026-01-26 12:16:13 +0700

    fix all 2

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -22,7 +21,7 @@ public class DifferTest {
     private static String expectedJson;
 
     @BeforeAll
-    static void init(@TempDir Path tempDir) throws IOException, URISyntaxException {
+    static void init() throws IOException, URISyntaxException {
         var jsonResourceUrl1 = DifferTest.class.getResource("/json_file_1.json");
         var jsonResourceUrl2 = DifferTest.class.getResource("/json_file_2.json");
         var yamlResourceUrl1 = DifferTest.class.getResource("/yaml_file_1.yaml");
@@ -38,29 +37,14 @@ public class DifferTest {
                     + "Ensure they are in src/test/resources/");
         }
 
-        Path jsonSourcePath1 = Paths.get(jsonResourceUrl1.toURI());
-        Path jsonSourcePath2 = Paths.get(jsonResourceUrl2.toURI());
-        Path yamlSourcePath1 = Paths.get(yamlResourceUrl1.toURI());
-        Path yamlSourcePath2 = Paths.get(yamlResourceUrl2.toURI());
+        jsonFilePath1 = Paths.get(jsonResourceUrl1.toURI()).toString();
+        jsonFilePath2 = Paths.get(jsonResourceUrl2.toURI()).toString();
+        yamlFilePath1 = Paths.get(yamlResourceUrl1.toURI()).toString();
+        yamlFilePath2 = Paths.get(yamlResourceUrl2.toURI()).toString();
+
         Path expectedStylishSourcePath = Paths.get(expectedStylishUrl.toURI());
         Path expectedPlainSourcePath = Paths.get(expectedPlainUrl.toURI());
         Path expectedJsonSourcePath = Paths.get(expectedJsonUrl.toURI());
-
-        // Копирование во временные файлы
-        Path jsonTempFile1 = tempDir.resolve("json_file_1.json");
-        Path jsonTempFile2 = tempDir.resolve("json_file_2.json");
-        Path yamlTempFile1 = tempDir.resolve("yaml_file_1.yaml");
-        Path yamlTempFile2 = tempDir.resolve("yaml_file_2.yaml");
-
-        Files.copy(jsonSourcePath1, jsonTempFile1);
-        Files.copy(jsonSourcePath2, jsonTempFile2);
-        Files.copy(yamlSourcePath1, yamlTempFile1);
-        Files.copy(yamlSourcePath2, yamlTempFile2);
-
-        jsonFilePath1 = jsonTempFile1.toString();
-        jsonFilePath2 = jsonTempFile2.toString();
-        yamlFilePath1 = yamlTempFile1.toString();
-        yamlFilePath2 = yamlTempFile2.toString();
         expectedStylish = Files.readString(expectedStylishSourcePath).trim();
         expectedPlain = Files.readString(expectedPlainSourcePath).trim();
         expectedJson = Files.readString(expectedJsonSourcePath).trim();
